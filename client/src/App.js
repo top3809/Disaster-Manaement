a51f22ac7c56d37b47c27c41894671f9105767d0
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2023-03-20 22:41:20 +0530

    new client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 
-import Home from "./components/home/Home";
-import "./styles/home.scss";
+import Home from "./components/admin/login";
+import "./styles/admin.scss";
 
 function App() {
   return (
---
+++ client/src/components/admin/login.jsx
@@ -0,0 +1,52 @@
+// QUe --> what is the difference between js and jsx
+// --> vscode khud b khud detect kar lega , if we use jsx ... or agar hm js use karnege to hme batna padega vscode ko ki hm react use kar rhe !!
+//prince kumar
+import React from 'react'
+import {motion} from "framer-motion";
+// import Founder from "./Founder";
+// import Menu from "./Menu";
+const Login= () => {
+
+  const options ={
+    initial:{
+      x:"-100%",
+      opacity:0,
+    },
+    whileInView:{
+      x:0,
+      opacity:1,
+    }
+  }
+  return (
+    <>
+      <section className='home'>
+        <div className='div'>
+        <motion.h1 {...options}>Sample Name</motion.h1>   
+        <motion.p {...options} 
+        transition={{
+          delay:0.2,
+        }} >some paragraph</motion.p>
+        </div>
+        <motion.a 
+        initial={{
+          y:"-100%",
+          opacity:0,
+        }}
+        whileInView={{
+          y:0,
+          opacity:1, 
+        }}
+        transition={{
+          delay:0.4,
+        }}
+        href='#menu'>
+            Explore Menu
+        </motion.a>
+    </section>
+    {/* <Founder/>
+    <Menu/> */}
+    </>
+  )
+}
+
+export default Login
