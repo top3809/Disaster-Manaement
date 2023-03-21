bf8cb50704073a876f6caafcfe9cb9e7c0ca2da4
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2023-03-22 00:08:48 +0530

    sign up and footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,22 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 
 import Login from "./components/admin/Login";
 import Signup from "./components/users/Signup";
-import "./styles/login.scss";
-import "./styles/signup.scss"
+import UserLogin from "./components/users/UserLogin";
+import Footer from "./components/layout/Footer";
 
+import "./styles/login.scss";
+import "./styles/signup.scss";
+import "./styles/userlogin.scss";
+import "./styles/footer.scss";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/admin" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/userlogin" element={<UserLogin/>}/>
       </Routes>
-
+      <Footer/>
     </Router>
   );
 }
