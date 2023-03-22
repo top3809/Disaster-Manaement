118d0197cfe70b378abe89fb39bf232a83222422
Prashant <pkg180901@gmail.com>
2023-03-22 19:27:38 +0530

    middleware error handling done

--- backend/user/routes/user.js
+++ backend/user/routes/user.js
@@ -1,6 +1,7 @@
 const express=require("express");
 
 const { registerUser,loginUser, logout} = require("../controllers/userController.js");
+const { isAuthenticatedUser} = require("../middleware/auth");
 
 const router=express.Router();
 
@@ -9,10 +10,12 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 router.route("/logout").get(logout);
+router.route("/home").get(isAuthenticatedUser);
 
+ 
 module.exports=router;   
 
 
  
-
+ 
 
