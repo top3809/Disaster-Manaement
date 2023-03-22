610d79657e6d96ec0b4c0eb77f5d5f2ec6168987
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2023-03-22 23:23:03 +0530

    intiated map.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/userlogin" element={<UserLogin />} />
             <Route path="/usersignup" element={<Signup />} />
+            
 
           </Routes>
         </AuthProvider>
--- client/src/components/user/Home.js
+++ client/src/components/user/Home.js
@@ -10,13 +10,12 @@ import hbcimg3 from '../../assets/homebigdata/hbc3.jpg'
 
 function Home() {
 
-  
 
   return (
     <div className='home-container'>
     
         <Homeslider></Homeslider>
-        <h1 className='slogan'>A spirited effort from our side can surely reduce the ill-effects of the Disaster</h1>
+
         <HomeBigCard hbcdata={hbcdata[0]} hbcimg={hbcimg1} hbcrow={hbcdata[0].row}></HomeBigCard>
         <HomeBigCard hbcdata={hbcdata[1]} hbcimg={hbcimg2} hbcrow={hbcdata[1].row}></HomeBigCard>
         <HomeBigCard hbcdata={hbcdata[2]} hbcimg={hbcimg3} hbcrow={hbcdata[2].row}></HomeBigCard>
